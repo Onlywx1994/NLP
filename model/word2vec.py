12efbfc961cbaf9c25b1140dbe7e72ccd93adcdf
wx <850040673@qq.com>
2019-08-21 23:13:18 +0800

    'word2vec'

--- model/word2vec.py
+++ model/word2vec.py
@@ -4,3 +4,21 @@ import  numpy as np
 
 import pandas as  pd
 
+import gensim
+
+
+from gensim.models import word2vec
+
+
+#word2vec的调用api实现
+
+data=word2vec.LineSentence()
+model=gensim.models.Word2Vec(sentences=data,size = 128, min_count = 3, iter = 50)
+#保存模型
+model.wv.save_word2vec_format("",binary = True)
+
+#加载模型
+word2vec=gensim.models.KeyedVectors.load_word2vec_format("", binary = True)
+
+
+#基于Tensorflow的word2vec实现
